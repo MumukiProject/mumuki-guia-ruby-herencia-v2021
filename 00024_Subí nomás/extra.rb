b11aed5f95522441afe45ade36fa68293d8e7396
gustavo@mumuki.org <gustavo@mumuki.org>
2021-02-26 19:05:04 +0000

    Mumuki Export on 2021-02-26 19:05:04 +0000

--- 00024_Subí nomás/extra.rb
+++ 00024_Subí nomás/extra.rb
@@ -1,1 +1,54 @@
-COMPLETAR!
+class Transporte
+	def initialize(combustible)
+		#<elipsis-for-student@
+		@combustible = combustible
+		#@elipsis-for-student>
+	end
+
+	def cargar_combustible!(combustible)
+		#<elipsis-for-student@
+		@combustible += combustible
+		#@elipsis-for-student>
+	end
+
+	def entran?(pasajeros)
+		#<elipsis-for-student@
+		self.maximo_personas >= pasajeros
+		#@elipsis-for-student>
+	end
+
+	def maxi_personas
+		#<elipsis-for-student@
+		@maximo_personas
+		#@elipsis-for-student>
+	end
+end
+
+class Auto < Transporte
+	def maximo_personas
+		#<elipsis-for-student@
+		5
+		#@elipsis-for-student>
+	end
+
+	def recorrer!(kilometros)
+		#<elipsis-for-student@
+		@litros -= kilometros / 2
+		#@elipsis-for-student>
+	end
+end
+
+class Moto < Transporte
+	def maximo_personas
+		#<elipsis-for-student@
+		2
+		#@elipsis-for-student>
+	end
+
+	def recorrer!(kilometros)
+		#<elipsis-for-student@
+		@litros -= kilometros
+		#@elipsis-for-student>
+	end
+end
+
