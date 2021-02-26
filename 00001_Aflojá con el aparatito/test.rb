782dcd60c5325c32206268c58a589cda16a9b31f
gustavo@mumuki.org <gustavo@mumuki.org>
2021-02-26 14:49:50 +0000

    Mumuki Export on 2021-02-26 14:49:50 +0000

--- 00001_Aflojá con el aparatito/test.rb
+++ 00001_Aflojá con el aparatito/test.rb
@@ -1,23 +1,29 @@
+class Celular
+  def bateria 
+    @bateria
+  end
+end
+
 celular = Celular.new
 
-it "Un auto inicializa con 40 litros de combustible" do
-  expect(auto.litros).to eq 40
+it "Un celular inicializa con 100 de bateria" do
+  expect(celular.bateria).to eq 100
 end
 
-it "Un auto no es ligero al iniciar" do
-  expect(auto.ligero?).to be false
+it "Un celular gasta 30 de batería si lo uso una hora" do
+  celular.cargar_a_tope!
+  celular.utilizar! 60
+  expect(celular.bateria).to eq 70
 end
 
-it "Un auto gasta 0.05 litros por cada kilómetro que se lo conduce" do
-  auto.conducir!(500)
-  expect(auto.litros).to eq (40 - (0.05 * 500))
+it "Un celular gasta 15 de batería si lo uso media hora" do
+  celular.cargar_a_tope!
+  celular.utilizar! 30
+  expect(celular.bateria).to eq 85
 end
 
-it "Un auto es ligero después de conducirlo muchos kilómetros" do
-  auto.ligero?
-  expect(auto.ligero?).to be true
-end
-
-it "Un auto tiene cuatro ruedas" do
-  expect(auto.cantidad_de_ruedas).to eq 4
+it "Un celular vuelve a tener 100 de batería si lo cargo a tope" do
+  celular.utilizar! 200
+  celular.cargar_a_tope!
+  expect(celular.bateria).to eq 100
 end
